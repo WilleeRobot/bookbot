2ed56b7dc7f589ab7becb99048d8b25d132e7fdc
Will L. <will@willlee.dev>
2024-05-23 19:51:14 -0600

    Added create report function

--- main.py
+++ main.py
@@ -12,14 +12,33 @@ def num_of_times_character_appears(text):
                 dict_of_characters[char] = 1
     return dict_of_characters
 
+def sort_on(dict):
+    return dict["count"]
+
 def main():
+    print_report()
+    
+def print_report():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
-    print(num_of_words(file_contents))
-    print(num_of_times_character_appears(file_contents))
-    
 
+    # Put into a dictionary the number of times each character appears in the document
+    letter_dict = num_of_times_character_appears(file_contents)
+    # Convert the dictionary into a list of dictionaries
+    list_of_characters = []
+    for char, count in letter_dict.items():
+        list_of_characters.append({"char":char, "count": count})
+    # Sort the list of dictionaries by the number of times the character appears
+    list_of_characters.sort(reverse=True, key=sort_on)
+    
+    # Report items
+    total_num_of_words = num_of_words(file_contents)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{total_num_of_words} words found in the document.")
+    print("")
+    for item in list_of_characters:
+        print(f"The '{item['char']}' character was found {item['count']} times.")
 
 
 main()
